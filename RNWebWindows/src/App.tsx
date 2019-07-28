95a23c5fabbb561cc3a0fc75d6615d09d0f19471
Marvin Heng <marvin.heng@haulio.io>
2019-07-28 21:58:52 +0800

    init

---
+++ RNWebWindows/src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { Router, Switch, Route } from './routing/routing';
+
+const instructions =
+  'Press Ctrl+R to reload,\n' +
+  'Shift+F10 or shake for dev menu';
+
+export default class App extends React.Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.tsx
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
